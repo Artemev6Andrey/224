1cc7c078b3c02d1e5e7b2526d8b5081cf10447d3
Andrey <aa7155443@gmail.com>
2022-05-10 21:19:33 +0300

    add new folder

---
+++ индивидуальные/2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Вывести на экран большее из трёх заданных чисел.
+a, b, c = int(input()), int(input()), int(input())
+mx = a
+if b > mx:
+    mx = b
+if c > mx:
+    mx = c
+print(mx)
