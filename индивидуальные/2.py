2229986b1c26a6cf008a17a56c454decb8f2a5ae
Andrey <aa7155443@gmail.com>
2022-05-26 02:17:38 +0300

    add new file

--- индивидуальные/2.py
+++ индивидуальные/2.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Вывести на экран большее из трёх заданных чисел"""
+
+"""
+Вывести на экран большее из трёх заданных чисел.
+"""
+
 if __name__ == '__main__':
     a, b, c = int(input()), int(input()), int(input())
     mx = a
+
     if b > mx:
         mx = b
     if c > mx:
